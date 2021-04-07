d99194d531453e851c234989094f1255dd13077f
HUANG SIZHE <hsz1273327@gmail.com>
2021-04-08 00:05:21 +0800

    update

--- buildimg.ps1
+++ buildimg.ps1
@@ -1,4 +1,4 @@
-docker buildx build --target=faiss-grpc-cpu --platform linux/amd64 -t hsz1273327/faiss-grpc-cpu:amd64-0.0.0 .
+docker buildx build --load --target=faiss-grpc-cpu --platform linux/amd64 -t hsz1273327/faiss-grpc-cpu:amd64-0.0.0 .
 # docker buildx build --target=faiss-grpc-cpu --platform linux/arm64 -t hsz1273327/faiss-grpc-cpu:arm64-0.0.0 .
 
 docker push -a  hsz1273327/faiss-grpc-cpu
